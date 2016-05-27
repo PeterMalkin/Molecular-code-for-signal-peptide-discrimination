42df35a687a1abac099b712d44f1144e9b885e9a
pe <pe@n0ne.info>
2016-05-26 20:00:45 -0700

    Protein feature table
    A script that sucks in all of the analyzed sequences,
    breaks them apart to a signal sequence and mature domain,
    then computes 57 scale scores for each of the sequence's
    signal and mature domain.

---
+++ protein_feature_table/calc_scales.py
@@ -0,0 +1,60 @@
+import os
+
+Acids = {
+        "Ala": "A",
+        "Arg": "R",
+        "Asn": "N",
+        "Asp": "D",
+        "Cys": "C",
+        "Gln": "Q",
+        "Glu": "E",
+        "Gly": "G",
+        "His": "H",
+        "Ile": "I",
+        "Leu": "L",
+        "Lys": "K",
+        "Met": "M",
+        "Phe": "F",
+        "Pro": "P",
+        "Ser": "S",
+        "Thr": "T",
+        "Trp": "W",
+        "Tyr": "Y",
+        "Val": "V"
+    }
+
+def parse_scale_file(filename):
+    global Acids
+    result = {}
+    with open(filename) as f:
+        contents = f.readlines()
+        
+    for line in contents:
+        s = line.split(",")
+        if (s):
+            result[Acids[s[0].strip()]] = float(s[1].strip())
+    return result
+
+def load_all_scale_tables(path="./scale_tables/"):
+    result = {}
+    for root, unused_dirs, files in os.walk(path):
+        for f in files:
+            if f.endswith(".txt"):
+                result[f[:f.find(".txt")]] = parse_scale_file(os.path.join(root, f))
+    return result
+
+def calc_scale(sequence, table):
+    scale = 0.0
+    for acid in sequence:
+        if acid not in table:
+            continue
+        scale += table[acid]
+        
+    avg = scale/len(sequence)
+    return avg
+
+if __name__ == "__main__":
+    all_scale_tables = load_all_scale_tables() 
+    for scale in all_scale_tables:
+        table = all_scale_tables[scale]
+        print scale + ": " + str(calc_scale("AAAAAAAAAAA", table))
---
+++ protein_feature_table/protein_features_table.py
@@ -0,0 +1,55 @@
+from calc_scales import load_all_scale_tables, calc_scale
+from load_protein_sequences import load_protein_sequences
+
+def get_table_heading(all_scale_tables):
+    result = []
+    result.append("accession")
+    result.append("signal sequence")
+    result.append("rest of sequence")
+    for scale in all_scale_tables:
+        result.append("signal sequence "+scale)
+        result.append("rest of sequence "+scale)
+    return result
+
+def calculate_features_table(all_scale_tables, all_sequences):
+    result = []
+    for item in all_sequences:
+        line = []
+        accession = item[0]
+        signal_seq = item[1]
+        rest_seq = item[2]
+         
+        line.append(accession)
+        line.append(signal_seq)
+        line.append(rest_seq)
+        
+        for scale in all_scale_tables:
+            table = all_scale_tables[scale]
+            scale_signal_seq = calc_scale(signal_seq, table)
+            scale_rest_seq = calc_scale(rest_seq, table)
+            line.append(scale_signal_seq)
+            line.append(scale_rest_seq)
+        
+        result.append(line)
+        del line
+    return result
+
+def save_table(table_headings, table_contents, filename = "features_table.csv"):
+    with open(filename,"w") as f:
+        for item in table_headings:
+            f.write(str(item)+",")
+        f.write("\n")
+        for items in table_contents:
+            for item in items:
+                f.write(str(item)+",")
+            f.write("\n")
+
+def Main():
+    all_scale_tables = load_all_scale_tables()
+    all_sequences = load_protein_sequences()
+    table_headings = get_table_heading(all_scale_tables)
+    table_contents = calculate_features_table(all_scale_tables, all_sequences)
+    save_table(table_headings, table_contents)
+
+if __name__ == "__main__":
+    Main()
